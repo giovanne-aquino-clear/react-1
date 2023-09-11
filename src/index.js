ee092568000218d86e1025ab79f4d5a8dec4a3c3
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-11 09:11:12 -0300

    add and use First.jsx

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './index.css'
 import ReactDOM  from "react-dom";
 import React from "react";
+import First from './components/basics/First'
 
 
 ReactDOM.render(
-    <div>Olá React! </div>,
+    <div>
+        Olá React!
+        <h1></h1>
+        <First></First>
+
+    </div>,
     document.getElementById('root')
 )
